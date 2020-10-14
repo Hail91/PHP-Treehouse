667a60edd29fa876db7808c6648a9b8904720b90
Hail91 <aarongillies88@gmail.com>
2020-10-14 11:54:44 -0400

    updated variables file with basic php variables

---
+++ PHP Basics/variables.php
@@ -0,0 +1,10 @@
+<?php
+// Some Basic practice with variables in PHP
+
+// 1.) Simple variable declarations in PHP for the four scalar (basic) types.
+$variable_int = 1; // <-- Assigned to a number
+$variable_string = 'I am a string'; // <--- Assigned to a string
+$variable_float = 2.5; // <--- Assigned to a float
+$variable_bool = true; // <--- Assigned to a boolean
+
+// 2.) Example of performing simple calculations on variables/data
