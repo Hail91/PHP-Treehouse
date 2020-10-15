df49eadb043711973a5d9eba85906590d9fbb0e5
Hail91 <aarongillies88@gmail.com>
2020-10-15 13:40:40 -0400

    added basic conditional check example in PHP

--- PHP Basics/arrays_controlStructs.php
+++ PHP Basics/arrays_controlStructs.php
@@ -1,3 +1,16 @@
 <?php
 
 // In this file, we'll have code examples of PHP Arrays and control structures.
+
+// ** Conditionals **
+// Simple if/elseif/else cascade with two variables.
+$a = 10;
+$b = '10';
+
+if ($a === $b) {
+    echo ' values are equal ';
+} elseif ($a < $b) {
+    echo ' $a is less than $b ';
+} elseif ($a > $b) {
+    echo ' $a is greater than $b ';
+} else echo ' values are NOT equal ';
