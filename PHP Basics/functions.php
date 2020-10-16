34bf24440d79d0c03ceb982c6d5fd55f8f40a0a6
Hail91 <aarongillies88@gmail.com>
2020-10-16 15:05:53 -0400

    added basic functions/anon function to php file

--- PHP Basics/functions.php
+++ PHP Basics/functions.php
@@ -1,3 +1,22 @@
 <?php
 
 // Some basic review of functions in PHP
+// Basic function call in PHP
+function add($x, $y)
+{
+    return $x + $y;
+};
+
+echo add(5, 10);
+echo "\n";
+// Global var to test anon function.
+$global_v = 'Aaron';
+
+// Anonymous function call (also referred to as a closure)
+// We will use a variable declared globally
+$greeting = function () use ($global_v) {
+    echo "Hello $global_v!";
+};
+
+echo $greeting();
+echo "\n";
