b4ea130e99f5b9e194106ab02bf8b3c999cf3ffa
Hail91 <aarongillies88@gmail.com>
2020-10-19 10:09:01 -0400

    added basic class with methods/properties in PHP

--- Object oriented PHP/classes.php
+++ Object oriented PHP/classes.php
@@ -2,13 +2,31 @@
     // Basic implentation of a class in PHP.
     class Recipe {
         // Properties
+        /* There are 3 types of access scopes on a class property 
+            - Public
+            - Private
+            - Protected
+        */
         public $title;
         public $ingredients = [];
         public $instructions = [];
         public $yield;
         public $tag = [];
         public $source = "Aaron Gillies";
+
+        // Methods
+        public function displayRecipe() {
+            return $this->title . " by " . $this->source;
+        }
     }
     $first_recipe = new Recipe();
-    // var_dump($first_recipe);
+    $first_recipe->title = "My first recipe";
+    echo $first_recipe->displayRecipe();
+    echo "\n";
+
+    $second_recipe = new Recipe();
+    $second_recipe->source = "New Person";
+    $second_recipe->title = "My second recipe";
+    echo $second_recipe->displayRecipe();
+    echo "\n";
 ?>
