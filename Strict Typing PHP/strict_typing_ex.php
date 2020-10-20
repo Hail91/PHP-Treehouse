2d3c94ea211bc1730306a86dacf1d87341a8f592
Hail91 <aarongillies88@gmail.com>
2020-10-20 15:17:39 -0400

    added basic example of strict typing in PHP7

---
+++ Strict Typing PHP/strict_typing_ex.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types = 1);
+// Example of strict typing as of PHP 7
+function getTotal(float $a, float $b) {
+    return $a + $b;
+}
+ // Int values will be converted to float, anything else will throw an Exception.
+echo getTotal(2.2, 3.3); 
+echo "\n";
